58692cf037568fc0025c15fbe453d5564868deb8
Dylan Hays <twentygx@outlook.com>
2023-01-31 20:08:26 -0600

    commit tree 99af025d03088eb65930c98cc6e176f4ae1f6409

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,21 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  response = case temperature
+    when temperature < 40 then "brisk"
+    when temperature >= 40 && temperature < 65 then "a little chilly"
+    when temperature >= 65 && temperature < 85 then "perfect"
+    when temperature >= 85 then "too dang hot"
+  end
+    return "It's #{response} out there!"
 end
 
 def fizzbuzz(num)
@@ -14,3 +26,4 @@ def calculator(operation, num1, num2)
   # your code here
 end
 
+hows_the_weather(35)
